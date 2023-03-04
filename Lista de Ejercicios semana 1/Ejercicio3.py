88dddc4444e8a69e12124b110789860c85aa3ffe
Ismael232 <126400070+Ismael232@users.noreply.github.com>
2023-03-04 02:29:57 -0500

    nuevo ejercicio

---
+++ Lista de Ejercicios semana 1/Ejercicio3.py
@@ -0,0 +1,12 @@
+x = []
+x.append(float(input("Ingrese el primer numero: ")))
+x.append(float(input("Ingrese el segundo numero: ")))
+suma = x[0] + x[1]
+resta = x[0] - x[1]
+division = x[0] / x[1]
+multiplicacion = x[0] * x[1]
+
+print(f"\nSuma + de los numeros: {suma}")
+print(f"Resta - de los numeros: {resta}")
+print(f"Division / de los numeros: {division}")
+print(f"Multiplicacion * de los numeros: {multiplicacion}")
