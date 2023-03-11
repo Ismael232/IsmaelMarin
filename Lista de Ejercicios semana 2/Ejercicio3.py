d07f43c06be8713fd1a9e9671a93ea1824ffb557
Ismael232 <126400070+Ismael232@users.noreply.github.com>
2023-03-11 17:58:10 -0500

    ejercicio3

---
+++ Lista de Ejercicios semana 2/Ejercicio3.py
@@ -0,0 +1,13 @@
+def mayorValor(x,y):
+    if x>y:
+        return x
+    elif x<y:
+        return y
+    else:
+        return "Los numeros son iguales"
+
+x = int(input("Escriba el primer valor: "))
+y = int(input("Escriba el segundo valor: "))
+
+valor = mayorValor(x,y)
+print(valor)
