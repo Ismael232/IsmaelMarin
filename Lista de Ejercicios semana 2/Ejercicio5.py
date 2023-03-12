6599150a9b32dc8bcda12215a51fd308cf183ade
Ismael232 <126400070+Ismael232@users.noreply.github.com>
2023-03-11 21:44:35 -0500

    n

---
+++ Lista de Ejercicios semana 2/Ejercicio5.py
@@ -0,0 +1,17 @@
+import os
+def archivosDeTuCarpeta(path):
+    aux = os.listdir(path)
+    for arch in aux:
+        rutaArchivos = os.path.join(path,arch)
+        if os.path.isdir(rutaArchivos) is False:
+           print(arch)
+        else:
+            archivosDeTuCarpeta(rutaArchivos)
+
+
+direccion = input("Ingrese la direccion de la carpeta: ")
+## Se agrega el valor r para concatenarlo a nuestra direccion
+direccionMejorada = r'{}'.format(direccion)  
+## Se usa este metodo ya que el programa no entiende bien la barra
+#la barra invertida \, y con esto hacemos entender que esa barra no sea ningun comando solo texto
+archivosDeTuCarpeta(direccionMejorada)
