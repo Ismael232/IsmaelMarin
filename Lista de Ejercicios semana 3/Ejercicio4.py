a509cddf46d1dcdafc13e7f32e322a620a97d63c
Ismael232 <126400070+Ismael232@users.noreply.github.com>
2023-03-18 03:37:58 -0500

    ejercicio4

---
+++ Lista de Ejercicios semana 3/Ejercicio4.py
@@ -0,0 +1,25 @@
+class Producto:
+    nombre = ""
+    codigo = ""
+
+    def __init__(self,nombre,codigo) -> None:
+        self.nombre = nombre
+        self.codigo = codigo
+    def desfragmentandoCodigo(self) -> str:
+        lista = []
+        pais = self.codigo.split("-",3)[0] 
+        lote = self.codigo.split("-",3)[1] 
+        year = self.codigo.split("-",3)[2]
+        lista.append(pais)
+        lista.append(lote)
+        lista.append(year)
+        return lista
+    
+    def __str__(self) -> str:
+        codigo = self.desfragmentandoCodigo()
+        return f"El producto proviene de {codigo[0]} y del lote {codigo[1]}"
+    
+producto1 = Producto("Camisa","Peru-0001-2013")
+print(producto1)
+
+
