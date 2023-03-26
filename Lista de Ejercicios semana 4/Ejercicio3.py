0bdf2e33cc3d111690d119010b9ee5739d1c1171
Ismael232 <126400070+Ismael232@users.noreply.github.com>
2023-03-25 19:54:39 -0500

    n

--- Lista de Ejercicios semana 4/Ejercicio3.py
+++ Lista de Ejercicios semana 4/Ejercicio3.py
@@ -1,13 +1,28 @@
 import requests
+
+
+
 def sunat():
     url = "https://api.apis.net.pe/v1/tipo-cambio-sunat"
     data = requests.get(url).json()
+    return data
 
-def pokemon():
+def pokedex():
     url = "https://pokeapi.co/api/v2/pokedex/1/"
     data = requests.get(url).json()
-    print(data)
+    return data
 
-url = "https://pokeapi.co/api/v2/pokemon"
-data = requests.get(url).json()
-print(data)
+def pokemon():
+    url = "https://pokeapi.co/api/v2/pokemon/"
+    data = requests.get(url).json()
+
+class casaCambio:
+    data = sunat()
+    compraActualizado = data["compra"]
+    ventaActualizado = data["venta"]
+    def compra(self,cantidadDollar):
+        cantidadSoles = cantidadDollar*self.compraActualizado
+        return cantidadSoles
+    def venta(self,cantidadSoles):
+        cantidadDollar = cantidadSoles/self.ventaActualizado
+        return cantidadDollar
--- Lista de Ejercicios semana 4/main.py
+++ Lista de Ejercicios semana 4/main.py
@@ -1,9 +1,94 @@
 from Ejercicio2 import sorteo
-
+import os
+"""
 sort = sorteo(100,6)
 sort.sorteoNumeros()
 print(sort.lista)
+"""
+from Ejercicio3 import *
 
+def borrarPantalla():
+    os.system('cls' if os.name=='nt' else 'clear')
 
+menu = """
+        --------MENU-----------
+        1)Sunat
+        2)Pokemon
+        3)Salir
+        """
+menuSunat = """
+        --------SUNAT-----------
+        1)Ver tipo de cambio
+        2)Comprar
+        3)Vender
+        4)Atras
+        5)Salir
+"""
+menuPokemon = """
+        --------POKEMON-----------
+        1)Listar pokemon
+        2)Buscar pokemon
+        2)Atras
+        3)Salir
+"""
+while True:
+    print(menu)
+    opcion = int(input("Selecione una opcion: "))
+    if opcion == 1:
+        borrarPantalla()
+        print(menuSunat)
+        opcion = int(input("Selecione una opcion: "))
+        if opcion == 1:
+            data = sunat()
+            compra = data["compra"]
+            venta = data["venta"]
+            print(f"La compra esta: {compra}")
+            print(f"La venta esta: {venta}")
+            input("Presione una tecla para volver al menu... ")
+            borrarPantalla()
+        if opcion == 2:
+            uno = casaCambio()
+            cantidad = float(input("Ingrese la cantidad de dolar que desea comprar: "))
+            soles = uno.compra(cantidad)
+            print(f"La cantidad de soles a pagar es: {soles} ")
+            input("Presione una tecla para volver al menu... ")
+            borrarPantalla()
+            continue
+        if opcion == 3:
+            uno = casaCambio()
+            cantidad = float(input("Ingrese la cantidad de soles que desea vender: "))
+            dollar = uno.venta(cantidad)
+            print(f"La cantidad de dolares a recibir es: {dollar} ")
+            input("Presione una tecla para volver al menu... ")
+            borrarPantalla()
+            continue
+        if opcion == 4:
+            borrarPantalla()
+            continue
+        if opcion == 5:
+            break
 
+    if opcion == 2:
+        borrarPantalla()
+        print(menuPokemon)
+        opcion = int(input("Selecione una opcion: "))
+        if opcion == 1:
+            data = pokedex()
+            lista = data['pokemon_entries']
+            for i,value in enumerate(lista):
+                nombre = value['pokemon_species']['name']
+                print(i,")",nombre)
+            input("Presione una tecla para volver al menu... ")
+            borrarPantalla()
+        if opcion == 2:
+            print("Funcion2")
+            input("Presione una tecla para volver al menu... ")
+            borrarPantalla()
+        if opcion == 3:
+            borrarPantalla()
+            continue   
+        if opcion == 4:
+            break
 
+    if opcion == 3:
+        break
