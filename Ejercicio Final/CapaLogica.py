1c344ec129d9e9d504f993eed1b5eb856e3d5b37
Ismael232 <126400070+Ismael232@users.noreply.github.com>
2023-04-02 17:55:07 -0500

    n

--- Ejercicio Final/CapaLogica.py
+++ Ejercicio Final/CapaLogica.py
@@ -7,16 +7,26 @@ def opcion(option):
             ruta = input("Indique la ruta: ")
             rutaMejorada = r'{}'.format(ruta)
             hoja = input("Indique el nombre de la hoja: ")
-            cargarArchivo(rutaMejorada,hoja)
+            df = cargarArchivo(rutaMejorada,hoja)
+            decision = input("Desea agregar una columna de precio total?[s/n]")
+            if decision.upper() == "S":
+                agregarPrecioTotal(df,hoja)
+            else:
+                print("No se realizaron cambios")
+            print(df)
         case 2:
             pass
         case 3:
-            pass
+            break
         case _:
             print("Opcion no valida")
 
 def cargarArchivo(ruta,hoja):
     df_excel = pd.read_excel(ruta,sheet_name=hoja)
     print(df_excel)
+    return df_excel
 
-
+def agregarPrecioTotal(dataframe,hoja):
+    dataframe["PrecioTotal"] = dataframe["Unidades vendidas"] * dataframe ["Precio unitario"]
+    rutaNueva = ".//PruebaCopia.xlsx"
+    dataframe.to_excel(rutaNueva,sheet_name= hoja,encoding='utf-8',index = False)
--- Ejercicio Final/CapaVisual.py
+++ Ejercicio Final/CapaVisual.py
@@ -1,10 +1,11 @@
 from CapaLogica import *
-print ("""
-        MENU DE OPERACIONES
-        1)Carga de un excel
-        2)Añadir columna
-        3)Insertar en base de datos
-        4)Leer base de datos
-        5)Crear tabla y registrar datos """)
-option = int(input("Elija una opcion : "))
-opcion(option)
+while True:
+    print ("""
+            MENU DE OPERACIONES
+            1)Carga de un excel
+            3)Insertar en base de datos
+            4)Leer base de datos
+            5)Crear tabla y registrar datos """)
+    option = int(input("Elija una opcion : "))
+    opcion(option)
+    
